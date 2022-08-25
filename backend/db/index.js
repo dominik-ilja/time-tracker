2eb0fd9af37bc89c77e6168182c41e2ee526bc5c
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-08-25 00:21:59 -0400

    Progress on resolving disconnect issue

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,29 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log('Error occurred: ', err);
+  }
+  else {
+    console.log('Connected to MySQL Server');
+  }
+});
+
+db.on('error', (err) => {
+  if (err) {
+    console.log('Error occurred: ', err);
+  }
+  else {
+    console.log('Connected to MySQL Server');
+  }
 });
 
 module.exports = db;
