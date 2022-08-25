cc5db6aa5e0850060e9e61c27cb4bf5a252b3841
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-08-25 00:45:44 -0400

    Heroku bug fix (#4)
    * Fixed Heroku H10 error code
    
    Error was fixed by changing `createConnection` to `createPool`. Rather than creating and managing connections one-by-one, the pooling module manages that for us.

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app = require('./app');
+require('dotenv').config();
 
 const listener = app.listen(process.env.PORT || 3006, () => {
   console.log('App is listening on port: ', listener.address().port);
