a32ac7f7a02f84767a7a2c4c30c16581051480d6
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-08-30 15:17:00 -0400

    Bug fixes (#13)
    * Bug fixes
    
    Updated formatLocalString util and the places it was referenced.

--- frontend/src/utils/formatLocalString.ts
+++ frontend/src/utils/formatLocalString.ts
@@ -1,14 +1,21 @@
 // "8/29/2022, 7:12:24 PM" -> "8-29-2022 7:12"
-export default function formatLocalString(time: string) {
-  const items = time.split(' ');
-  const isNight = items[2] === 'PM';
+export default function formatLocalString(date: Date) {
+  const items = date.toLocaleString().split(' ');
+  const isMorning = items[2] === 'AM';
   const digits = items[1].split(':');
   const dates = items[0].replace(',', '').split('/');
+  const hour = Number.parseInt(digits[0]);
+  const cycleHours = 12;
+  let militaryHour: number;
 
-  if (isNight) {
-    const nightTime = Number.parseInt(digits[0]) + 12;
-    digits[0] = nightTime === 24 ? '00' : nightTime.toString();
+  if (isMorning) {
+    militaryHour = hour === cycleHours ? hour - cycleHours : hour;
   }
+  else {
+    militaryHour = hour === cycleHours ? hour : hour + cycleHours;
+  }
+
+  digits[0] = militaryHour < 10 ? '0' + militaryHour.toString() : militaryHour.toString();
 
   return `${dates[2]}-${dates[0]}-${dates[1]} ${digits[0]}:${digits[1]}`;
 }
